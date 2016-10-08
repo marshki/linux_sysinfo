1c9610985b6e52b27173193239a9929e565a2bf5
marshki <marshall@morethantomorrow.org>
2016-10-08 14:24:18 -0400

    Fix spacing, typos.

--- system_info.sh
+++ system_info.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # This script allows you to extract useful information from a *NIX host using a menu-driven interface. 
 
-# Define Variables 
+# Define variables 
 LSB=/usr/bin/lsb_release 
 
 # Display pause prompt
-# $1-> Message
+# $1-> Message (optional)
 function pause(){
     local message="$@"
-    [ -z $message ] && message="Press [Enter] key to continue..."
+    [ -z $message ] && message="Press [Enter] key to continue."
     read -p "$message" readEnterKey
 }
 
@@ -16,10 +16,10 @@ function pause(){
 function show_menu(){
     date
     echo ""
-    echo "  Main Menu   "
+    echo "      Main Menu"
     echo ""
-        echo "1. OS Info" 
-        echo "2. Exit " 
+        echo "  1. OS Info" 
+        echo "  2. Exit " 
 }
 
 # Display header message 
@@ -31,9 +31,9 @@ function write_header(){
     echo ""
 }
 
-# Get info about OS 
+# Get info about Operating System 
 function  os_info(){
-    write_header "System Information"
+    write_header "  System Information"
     echo "Operating System : $(uname)"
     [ -x $LSB ] && $LSB -a || echo "$LSB command is not installed (set \$LSB variable)"
     # pause "Press [Enter] key to continue..."
