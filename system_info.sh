3cfe86d08c0cb597aef42328d4c0c1fa04f2e549
marshki <marshall@morethantomorrow.org>
2016-10-09 08:23:47 -0400

    Add user_info function

--- system_info.sh
+++ system_info.sh
@@ -75,6 +75,16 @@ function net_info(){
     pause 
 }
 
+# Display a list of users currently logged on & a list of recently logged in users
+
+function user_info(){
+    local cmd="$1"
+    case "$cmd" in
+        who) write_header " Who is online? "; who -H; pause ;;
+        last) write_header " List of last logged in users "; last; pause ;;
+    esac
+}
+
 # Get input via the keyboard and make a decision using case..esac 
 function read_input(){
     local c
