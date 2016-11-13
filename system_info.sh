387344d8e7c830df3d45b5e040860b6319089f0d
marshki <marshall@morethantomorrow.org>
2016-11-13 18:06:58 -0500

    update ps info

--- system_info.sh
+++ system_info.sh
@@ -104,6 +104,7 @@ function user_info(){
 # Display used and free memory info 
 
 function mem_info(){
+        local processes=$(ps -Ao user,pid,pcpu,pmem,stat,command --sort=-%mem,-%cpu) 
         write_header "Free & Used Memory "
         free --giga --human                         				# free -gh  
 
@@ -115,8 +116,11 @@ function mem_info(){
 echo "***********************************"
 echo "*** Top 5 Memory Eating Process ***"
 echo "***********************************"
-        ps aux |awk 'NR==1{print $0}'                                   # get header row from ps
-        ps auxf |sort --numeric-sort --reverse --key=4 |head -5|awk '{ print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13}'
+        # ps -Ao user,pid,pcpu,pmem,stat,command --sort=-%mem,-%cpu | 
+        echo '${processes}'# | head -6 | awk '{print $1, $2, $3, $4, $5, $6, $6, $7}'
+
+        #ps aux |awk 'NR==1{print $0}'                                   # get header row from ps
+        #ps auxf |sort --numeric-sort --reverse --key=4 |head -5|awk '{ print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13}'
   		# sort -nr -k 4 | head -5           
         pause
 }
