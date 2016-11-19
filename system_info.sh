57bb7a063248a9e54dcb1462dd240196af31265a
marshki <marshall@morethantomorrow.org>
2016-11-19 13:52:17 -0500

    spacing of echo statements

--- system_info.sh
+++ system_info.sh
@@ -112,16 +112,16 @@ function mem_info(){
         write_header "Free & Used Memory "
         free --giga --human                         				# free -gh  
 
-echo "***********************************"
-echo "***  Virtual Memory Statistics  ***"
-echo "***********************************"
-        vmstat
+    echo "***********************************"
+    echo "***  Virtual Memory Statistics  ***"
+    echo "***********************************"
+    vmstat
 
-echo "***********************************"
-echo "*** Top 5 Memory Eating Process ***"
-echo "***********************************"
-        echo "${processes}" | head -6 |awk '{print $1, $2, $3, $4, $5, $6, $6, $7}'
-        pause
+    echo "***********************************"
+    echo "*** Top 5 Memory Eating Process ***"
+    echo "***********************************"
+    echo "${processes}" | head -6 |awk '{print $1, $2, $3, $4, $5, $6, $6, $7}'
+    pause
 }
 
 # Get input via the keyboard and make a decision using case..esac 
