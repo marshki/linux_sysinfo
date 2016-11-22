2abbde551354dcc2ca6aaff31c496a4acfd30040
marshki <marshall@morethantomorrow.org>
2016-11-21 19:54:33 -0500

    10 last logged in users; who with column headers

--- osx_user_info.sh
+++ osx_user_info.sh
@@ -6,7 +6,7 @@ function user_info(){
     local cmd="$1"
     case "$cmd" in
         who) write_header " Who is online? "; who -H; pause ;;
-        last) write_header " List of last logged in users "; last; pause ;;
+        last) write_header " Last 10 logged in users "; last -10; pause ;;
     esac
 }
 
