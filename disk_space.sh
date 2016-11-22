df885d10f4328784df863ffe2ae3ca17a7a60a4d
marshki <marshall@morethantomorrow.org>
2016-11-22 10:59:51 -0500

    More verbose output for disk_space

--- disk_space.sh
+++ disk_space.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 # Function to retrieve disk space and 10 largest files on system 
 
-
 function disk_space(){
-    find / -type f -exec du -Sh {} + | sort -rh | head -n 10 
+    find / -type f -exec du -Sh {} + | sort --reverse --human| head --lines=10 
+    # find / -type f -exec du -Sh {} + | sort -rh | head -n 10
 }
+
 disk_space
