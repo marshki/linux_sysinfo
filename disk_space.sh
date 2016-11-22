eda5e50101d2d59718f23a3ec397132449f7cdf0
marshki <marshall@morethantomorrow.org>
2016-11-22 10:35:09 -0500

    Initial disk space function commit

--- disk_space.sh
+++ disk_space.sh
@@ -1,3 +1,8 @@
 #!/bin/bash 
 # Function to retrieve disk space and 10 largest files on system 
 
+
+function disk_space(){
+    find / -type f -exec du -Sh {} + | sort -rh | head -n 10 
+}
+disk_space
