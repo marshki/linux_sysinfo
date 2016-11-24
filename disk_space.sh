d445470c4ec4b7af2cca42a8c5ae620c07492b85
marshki <marshall@morethantomorrow.org>
2016-11-23 22:42:27 -0500

    comment

--- disk_space.sh
+++ disk_space.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 # Function to retrieve disk space and 10 largest files on system 
+# For this to work correctly, user needs elevated privileges?
+
 
 function disk_space(){
-    find / -type f -exec du --separate-dirs --human-readable {} + | sort --reverse --human| head --lines=10 
+    find / -type f -exec du --separate-dirs --human-readable {} + | sort --reverse --human | head --lines=10 
     # find / -type f -exec du -Sh {} + | sort -rh | head -n 10
 }
 
