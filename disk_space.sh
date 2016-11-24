5d0d5747e76429f24b454cc89c720261bc67c869
marshki <marshall@morethantomorrow.org>
2016-11-23 20:14:51 -0500

    make function more exlicit

--- disk_space.sh
+++ disk_space.sh
@@ -2,7 +2,7 @@
 # Function to retrieve disk space and 10 largest files on system 
 
 function disk_space(){
-    find / -type f -exec du -Sh {} + | sort --reverse --human| head --lines=10 
+    find / -type f -exec du --separate-dirs --human-readable {} + | sort --reverse --human| head --lines=10 
     # find / -type f -exec du -Sh {} + | sort -rh | head -n 10
 }
 
