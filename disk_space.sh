641cb9b070e9fe0b27110dc902e5d2a7fe712f1e
marshki <marshall@morethantomorrow.org>
2016-11-25 21:06:37 -0500

    even more baked in disk space goodness

--- disk_space.sh
+++ disk_space.sh
@@ -5,11 +5,11 @@
 
 function disk_space(){
     local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null) 
-    # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null         
-    
+    # find largest files by disk space; output background noise to /dev/null 
+    # find / -type f -exec du -Sh {} + 2>/dev/null
+          
     echo "Retrieving largest files..."
-    echo "${largestfiles}" |sort --reverse --human | head --lines=10
-    # sort -rh | head -n 10
+    echo "${largestfiles}" |sort --reverse --human | head --lines=10                # sort -rh | head -n 10
  
     # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | sort --reverse --human | head --lines=10 
     # find / -type f -exec du -Sh {} + | sort -rh | head -n 10
