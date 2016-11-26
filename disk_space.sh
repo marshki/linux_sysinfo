f2cbaaf19049c53610ba9bb72b011dcb26189766
marshki <marshall@morethantomorrow.org>
2016-11-25 20:56:39 -0500

    more disk space goodness

--- disk_space.sh
+++ disk_space.sh
@@ -4,11 +4,12 @@
 # Bare-bones 
 
 function disk_space(){
-    echo "Retrieving results..."
-    local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | sort --reverse --human |
-    head --lines=10)
-       
-    echo "${largestfiles}"   
+    local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null) 
+    # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null         
+    
+    echo "Retrieving largest files..."
+    echo "${largestfiles}" |sort --reverse --human | head --lines=10
+    # sort -rh | head -n 10
  
     # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | sort --reverse --human | head --lines=10 
     # find / -type f -exec du -Sh {} + | sort -rh | head -n 10
