dc4dd41f92e0bc79a762cb3057d8ba54104ac61f
marshki <marshall@morethantomorrow.org>
2016-11-25 21:38:42 -0500

    further refinements on disk_space, need to fix lag

--- disk_space.sh
+++ disk_space.sh
@@ -1,12 +1,29 @@
 #!/bin/bash 
 #### Display information on disk space usage ####
 
+#### Display header message ####                                                                                                      
+# $1 - message                                                                                                                        
+                                                                                                                                      
+function write_header(){                                                                                                              
+    local h="$@"                                                                                                                      
+    echo "------------------------------"                                                                                             
+    echo "  ${h}"                                                                                                                     
+    echo "------------------------------"                                                                                             
+} 
+
 function disk_space(){
     local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null) 
     # find largest files by disk space; output background noise to /dev/null 
     # find / -type f -exec du -Sh {} + 2>/dev/null
-          
+         
+    write_header "Disk Usage" 
+    df --human-readable --total                                                     # df -h --total  
+    
     echo "Retrieving largest files..."
+    
+    echo "------------------------------" 
+    echo "   Top 10 Disk Eating Files   " 
+    echo "------------------------------"     
     echo "${largestfiles}" |sort --reverse --human | head --lines=10                # sort -rh | head -n 10
  
     # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | sort --reverse --human | head --lines=10 
