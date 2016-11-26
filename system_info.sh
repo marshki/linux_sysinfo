61559d020c763e2489c97f193feb36ff240093ef
marshki <marshall@morethantomorrow.org>
2016-11-26 08:58:34 -0500

    Add disk_space function

--- system_info.sh
+++ system_info.sh
@@ -134,6 +134,28 @@ function mem_info(){
 
 #### Display information on disk space usage ####
 
+
+function disk_space(){
+    local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null)
+    # find largest files by disk space; output background noise to /dev/null 
+    # find / -type f -exec du -Sh {} + 2>/dev/null
+
+    write_header "Disk Usage"
+    df --human-readable --total | awk 'NR==1; END{print}'                           # df -h --total   
+
+    # echo "Retrieving largest files..."
+
+    echo "------------------------------" 
+    echo "   Top 10 Disk Eating Files   " 
+    echo "------------------------------"     
+    echo "${largestfiles}" |sort --reverse --human | head --lines=10                # sort -rh | head -n 10
+
+    # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | sort --reverse --human | head --lines=10 
+    # find / -type f -exec du -Sh {} + | sort -rh | head -n 10
+}
+
+
+
 #### Get input via the keyboard and make a decision using case..esac ####
 
 function read_input(){
