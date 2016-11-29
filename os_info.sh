e3aba22baa49e99f2086e412ec1f7271a2c577bf
marshki <marshall@morethantomorrow.org>
2016-11-28 20:40:27 -0500

    Comment os_info on suggestion for output format

--- os_info.sh
+++ os_info.sh
@@ -19,8 +19,10 @@ function  os_info(){
     write_header "System Info"
     echo "Operating System : $(uname --kernel-name)"                # uname -s 
     echo "Kernel Version   : $(uname --kernel-release)"             # uname -r 
-    awk '/^NAME=/||/^VERSION=/' /etc/os-release 
-    
+    awk '/^NAME=/||/^VERSION=/' /etc/os-release                     # Not perfect, but OK. Let's print output so that we 
+                                                                    # match formatting for the earlier echo statements, e.g.
+                                                                    # Name   :"Debian GNU/Linux"
+                                                                    # Version:"8 (jessie)" 
     # [ -x $LSB ] && $LSB -a || echo "$LSB command is not installed (set \$LSB variable)"
     # cat /etc/os-release 
 }
