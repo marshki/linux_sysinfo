0693e2e4aa1478029f20c5c4a11a82e48a93b85d
marshki <marshall@morethantomorrow.org>
2016-11-29 20:06:47 -0500

    not quite there yet

--- os_info.sh
+++ os_info.sh
@@ -20,14 +20,8 @@ function  os_info(){
     echo "Operating System : $(uname --kernel-name)"                # uname -s 
     echo "Kernel Version   : $(uname --kernel-release)"             # uname -r 
     awk '/^NAME=/||/^VERSION=/' /etc/os-release 
-    # awk -F'=' '{print $2}' /etc/os-release 
-    cut -d'=' -f2 /etc/os-release.extra                                                                     # Not perfect, but OK. Let's print output so that we 
-    # awk -F'=' '{print $2}' /etc/os-release                        # match formatting for the earlier echo statements, e.g.
-                                                                    # Name   :"Debian GNU/Linux"
-                                                                    # Version:"8 (jessie)" 
-                                                                    
+}     
+                                                                   
     # [ -x $LSB ] && $LSB -a || echo "$LSB command is not installed (set \$LSB variable)"
-    # cat /etc/os-release 
-}
 
 os_info 
