85e2dddbe22ddb6531cfbca1db7d9286f0e1eb89
marshki <marshall@morethantomorrow.org>
2016-11-30 20:44:19 -0500

    Not exactly as I''d like, but close

--- os_info.sh
+++ os_info.sh
@@ -1,7 +1,6 @@
 #### Define Variables ####
 # Linux Standard Base Module (LSB) provides distro-specific info
-
-LSB=/usr/bin/lsb_release
+# LSB=/usr/bin/lsb_release
 
 #### Display header message ####
 # $1 - message
@@ -16,10 +15,16 @@ function write_header(){
 #### Get info about Operating System ####
 
 function  os_info(){
+    #local namevers=$(awk -F'[="]+' '/^(NAME|VERSION)=/{printf("%-17s: %s\n",$1,$2)}' /etc/os-release)  
+    local name=$(awk -F'[="]+' '/^(NAME)=/{printf("%-17s: %s\n",$1,$2)}' /etc/os-release)
+    local vers=$(awk -F'[="]+' '/^(VERSION)=/{printf("%-17s: %s\n",$1,$2)}' /etc/os-release)
     write_header "System Info"
-    echo "Operating System : $(uname --kernel-name)"                # uname -s 
-    echo "Kernel Version   : $(uname --kernel-release)"             # uname -r 
-    awk -F'[="]+' '/^(NAME|VERSION)=/{printf("%-17s: %s\n",$1,$2)}' /etc/os-release
+    #echo "Operating System : $(uname --kernel-name)"                # uname -s 
+    #echo "Kernel Version   : $(uname --kernel-release)"             # uname -r 
+    #awk -F'[="]+' '/^(NAME|VERSION)=/{printf("%-17s: %s\n",$1,$2)}' /etc/os-release
+    echo ${name}
+    echo ${vers}
+    #echo ${namevers}
 }    
                                                                    
     # [ -x $LSB ] && $LSB -a || echo "$LSB command is not installed (set \$LSB variable)"
