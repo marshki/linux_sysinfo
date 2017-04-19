baaed8619e39534044f0240e0c185adb8efb8b0f
marshki <marshall@morethantomorrow.org>
2017-04-19 11:33:39 -0400

    regex verbose example

--- cpu_info.sh
+++ cpu_info.sh
@@ -14,13 +14,15 @@ function write_header(){
 #### Get info about Operating System ####
 
 function  cpu_info(){
-    local cpu=$(cat /proc/cpuinfo | grep -m 1 -i 'model name' | awk '{$1=$2=$3=""; print $0}')  
+    local cpu=$(cat /proc/cpuinfo | grep --max-count=1 --ignore-case 'model name' | awk '{$1=$2=$3=""; print $0}') # grep -m 1 -i 'model name' 
         
     write_header "CPU Info"
     echo "CPU  : ${cpu}"          
-    echo "Number of Cores: $(getconf _NPROCESSORS_ONLN)"  
+    echo "Cores: $(getconf _NPROCESSORS_ONLN)"  
+
     # printf '%s\n' "$cpu"
 
+    # pause 
 
 }                                                                      
     
