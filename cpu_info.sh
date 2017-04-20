602567e576247c24f89de3501b5d0091209a63ff
marshki <marshall@morethantomorrow.org>
2017-04-20 15:44:21 -0400

    update cpu_info.sh

--- cpu_info.sh
+++ cpu_info.sh
@@ -13,14 +13,12 @@ function write_header(){
 #### Get info about CPU ####
 
 function  cpu_info(){
-    # local cpu=$(cat /proc/cpuinfo |grep --max-count=1 --ignore-case 'model name' |awk '{$1=$2=$3=""; print $0}') # grep -m 1 -i '' 
-    # regexp on /proc/cpuinfo to extract 1st match of CPU `model name`, then print everything to the right of 3rd column     
     
     write_header "CPU Info"
     lscpu |grep --ignore-case 'model name'
     lscpu |grep --ignore-case 'socket(s)'          
     lscpu |grep --ignore-case 'core(s) per socket'     
-    # Query system configuration for number of cores 
+    # Query lscpu for: `model name`, `sockets`, and `numb
 
     # pause 
 
