0e9e1113cac36d633673d6028b068faba09f5105
marshki <marshall@morethantomorrow.org>
2017-05-01 18:51:58 -0400

    update progressbar.sh

--- progressbar.sh
+++ progressbar.sh
@@ -1,13 +1,17 @@
 #!/bin/bash 
-# Possible progress basr 
+# Possible progress bar function 
 
-BAR='####################'      # make it full, this is 20 chars
+function progress_bar(){
+    
+    BAR='####################'      # make it full, this is 20 chars
 
-for i in {1..20}; do
-    echo -ne "\r${BAR:0:$i}"    # print $i chars of $BAR from 0 position
-    sleep 0.5                   # sleep one second, por favor              
-done
+    for i in {1..20}; do
+        echo -ne "\r${BAR:0:$i}"    # print $i chars of $BAR from 0 position
+        sleep 0.5                   # sleep one second, por favor              
+    done
+} 
 
+progress_bar   
 
                                 # echo -n - print without new line at the end
                                 # echo -e - interpret special characters while printing
