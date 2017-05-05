e0f766877ded5af0e84e5158d509d9ae04891f47
marshki <marshall@morethantomorrow.org>
2017-05-05 09:45:08 -0400

    update grr.sh

--- grr.sh
+++ grr.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo -n "Please wait..."
- while true
- do
-      echo -n "."
-      sleep .2
- done
+function wait_message() { 
+    echo -n "Please wait..."
+    while true
+    do
+        echo -n "."
+        sleep .2
+    done
+} 
 
+wait_message 
