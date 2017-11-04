c3ec34d6022b0c4b092b3ac6c391d09dbcd95810
marshki <marshall@morethantomorrow.org>
2017-11-04 14:23:46 -0400

    add template.sh

---
+++ functions/template.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#Template for creation of header and function 
+
+#### Display header message #### 
+# $1 - message 
+
+function write_header(){
+    local h="$@"
+    echo "------------------------------"
+    echo "  ${h}"
+    echo "------------------------------"
+}
+
+#### Describe function ####
+
+function(){
+
+    # pause 
+}
+
+function
