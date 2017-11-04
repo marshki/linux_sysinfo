da8e394928a213fed5bfaada1c57f951055f62aa
marshki <marshall@morethantomorrow.org>
2017-11-04 12:52:58 -0400

    update disk_space.sh

---
+++ functions/disk_space.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+#Need to run as root 
+
+
+#### Display header message ####                                                                                                      
+# $1 - message                                                                                                                        
+                                                                                                                                      
+function write_header(){                                                                                                              
+    local h="$@"                                                                                                                      
+    echo "------------------------------"                                                                                             
+    echo "  ${h}"                                                                                                                     
+    echo "------------------------------"                                                                                             
+} 
+
+# Get information about free and used disk space 
+
+function disk_space(){
+    local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null) 
+    # find largest files by disk space; output background noise to /dev/null 
+    # find / -type f -exec du -Sh {} + 2>/dev/null
+         
+    write_header "Disk Usage" 
+    df --human-readable --total | awk 'NR==1; END{print}'                           # df -h --total   
+    
+    # echo "Retrieving largest files..."
+    
+    echo "------------------------------" 
+    echo "   Top 10 Disk Eating Files   " 
+    echo "------------------------------"     
+    echo "${largestfiles}" | pv | sort --reverse --human | head --lines=10                # sort -rh | head -n 10
+ 
+    # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | sort --reverse --human | head --lines=10 
+    # find / -type f -exec du -Sh {} + 2>/dev/null | sort -rh | head -n 10
+}
+
+disk_space
