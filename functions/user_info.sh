7cefef43b9a41dcfca99b79ad51e941c98d0b090
marshki <marshall@morethantomorrow.org>
2017-11-04 14:55:51 -0400

    update permissions

---
+++ functions/user_info.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+#### Display header message #### 
+# $1 - message 
+
+function write_header(){
+    local h="$@"
+    echo "------------------------------"
+    echo "  ${h}"
+    echo "------------------------------"
+}
+
+#### Display list of users currently logged on & a list of recently logged in users ####
+
+function user_info(){
+
+    write_header "User Info"
+
+    local cmd="$1"
+    case "$cmd" in
+        who) write_header "Who is Online? "; who --heading;;                # who -H 
+        last) write_header "Last 10 Logged in Users "; last -n 10 -a -d;;   # last -num 10 -a -d   
+    esac
+    
+    # pause 
+}
+
+user_info
