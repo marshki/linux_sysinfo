1b18f91270d8cccd4207486f56e3c96728833976
marshki <marshall@morethantomorrow.org>
2017-12-02 16:09:00 -0500

    provide user feedback

--- functions/disk_space.sh
+++ functions/disk_space.sh
@@ -1,11 +1,6 @@
 #!/bin/bash 
 #Need to run as root 
 
-# TODO: The pv command waits for the find command to finish 
-# before it provides output--this makes it look like nothing 
-# is happening when going through large data sets. 
-# Can we pipe find to pv to get some feedback? 
-
 #### Display header message ####                                                                                                      
 # $1 - message                                                                                                                        
 function write_header(){                                                                                                              
@@ -18,15 +13,16 @@ function write_header(){
 # Get information about free and used disk space 
 
 function disk_space(){
-    local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null)
+
+    printf "%s\n" "Searching..." 
+
+    local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | pv)
     
     # find largest files by disk space; output background noise to /dev/null 
     # find / -type f -exec du -Sh {} + 2>/dev/null
-    
-    printf "%s\n" "Searching..." 
-     
+             
     write_header "Disk Usage" 
-    df --human-readable --total | awk 'NR==1; END{print}'                           # df -h --total   
+    df --human-readable --total | awk 'NR==1; END{print}'                                          # df -h --total   
         
     printf "%s\n" "------------------------------" 
     printf "%s\n" "   Top 10 Disk Eating Files   " 
