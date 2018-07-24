8488b3a9b6c079dabb758a4622fcb7b183c25840
marshki <marshall@morethantomorrow.org>
2018-07-24 12:50:32 -0400

    refactor in progress

--- functions/os_info.sh
+++ functions/os_info.sh
@@ -1,25 +1,27 @@
 #!/bin/bash 
 
 #### Display header message ####
-# $1 - message
 
-function write_header(){
-    local h="$@"
-    printf "%s\n" "------------------------------"
-    printf "%s\n" "  ${h}"
-    printf "%s\n" "------------------------------"
+write_header() {
+  local name=$1; shift;
+  printf "%s\\n""--------------------\\n$name%s\\n--------------------\\n"
+  printf "%s\\n" "$@"
 }
 
 #### Get info about Operating System ####
 
 function  os_info(){
-        
-    write_header "System Info"
-    printf "%s\n" "Operating System : $(uname --kernel-name)"                # uname -s 
-    printf "%s\n" "Kernel Version   : $(uname --kernel-release)"             # uname -r
-    printf "%s\n" "Name             : $(cat /etc/*-release |grep --word-regexp "NAME="   |sed 's/NAME=//g; s/"//g')"
-    printf "%s\n" "Version          : $(cat /etc/*-release |grep --word-regexp "VERSION="|sed 's/VERSION=//g; s/"//g')" 
+  local os=$(uname --kernel-name)
+  local kernel=$(uname --kernel-release)
+  local name=$()        
+  local version=$()    
 
+printf "%s\n" "Operating System : $(uname --kernel-name)"                # uname -s 
+printf "%s\n" "Kernel Version   : $(uname --kernel-release)"             # uname -r
+printf "%s\n" "Name             : $(cat /etc/*-release |grep --word-regexp "NAME="   |sed 's/NAME=//g; s/"//g')"
+printf "%s\n" "Version          : $(cat /etc/*-release |grep --word-regexp "VERSION="|sed 's/VERSION=//g; s/"//g')" 
+
+  write_header "System Info" "$os_info" 
 }                                                                      
-    
+# awk -F= '{print $2}' /etc/*release    
 os_info 
