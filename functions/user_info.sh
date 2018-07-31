4da86784fea6dadabd4866e659051cfefacf63b9
marshki <marshall@morethantomorrow.org>
2018-07-31 13:16:40 -0400

    add header and info functions

--- functions/user_info.sh
+++ functions/user_info.sh
@@ -1,17 +1,24 @@
-#!/bin/bash 
+#!/bin/bash
+ 
+#### Print header ####
+
+write_header() {
+  local name=$1; shift;
+  printf "%s""--------------------\\n$name%s\\n--------------------\\n"
+  printf "%s" "$@"
+}
+
+#### Print info ####
+
+write_info() {
+  local name=$1; shift;
+  printf "%s""$name%s"
+  printf "%s\\n" "$@"
+}
 
 # TODO: Refactor 
 # TODO: Works as part of the entire program, should modify to stand on its own 
  
-#### Display header message #### 
-# $1 - message 
-
-function write_header(){
-    local h="$@"
-    printf "%s\n" "------------------------------"
-    printf "%s\n" "  ${h}"
-    printf "%s\n" "------------------------------"
-}
 
 #### Display list of users currently logged on & a list of recently logged in users ####
 
