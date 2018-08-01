505abc72be74c4ae4e38c6d79b4cbc140f9f411d
marshki <marshall@morethantomorrow.org>
2018-08-01 14:32:14 -0400

    split user_info in to two functions

---
+++ functions/recent_user.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#### Display a list of recently logged in users ####
+ 
+#### Print header ####
+
+write_header() {
+  local name=$1; shift;
+  printf "%s""--------------------\\n$name%s\\n--------------------\\n"
+  printf "%s" "$@"
+}
+
+#### Print info ####
+
+write_info() {
+  local name=$1; shift;
+  printf "%s""$name%s"
+  printf "%s\\n" "$@"
+}
+
+ten_last() { 
+  # Display 10 last logged in users
+
+  local lasty=$(last --limit 10 --hostlast --dns)  
+  write_info "Last 10 logged in Users: ${lasty}"
+} 
+
+recent_user() { 
+  # wrapper function 
+  write_header "USER INFO" "$user_info" 
+
+  ten_last 
+} 
+
+recent_user 
