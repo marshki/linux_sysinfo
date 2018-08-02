64a7ad36d87aebece64eb72d5f25afff40e15dcd
marshki <marshall@morethantomorrow.org>
2018-08-01 22:47:58 -0400

    refactore; still needs work

--- functions/mem_info.sh
+++ functions/mem_info.sh
@@ -19,42 +19,63 @@ write_info() {
 #### Describe function ####
 
 ram_stats() {
-  # free & used memory 
+  # display free & used memory 
 
-  local freey=$(free --giga --human) 
-  write_info "Free & Used Memory: ${freey}"
+  local ram=$(free --giga --human) 
+  write_info "Free & Used Memory: ${ram}"
 }
 
-#### Display used and free memory info ####
-function mem_info(){
-    local processes=$(ps -Ao user,pid,pcpu,pmem,stat,command --sort=-%mem,-%cpu) 
-    # regex ps to define, extract, and sort top memory (then cpu) consuming processes  
-        
-    write_header "Free & Used Memory "        
-    free --giga --human                                                 # free -gh  
-    
-    printf "%s\n" "------------------------------"
-    printf "%s\n" "  Virtual Memory Statistics   "
-    printf "%s\n" "------------------------------"
-    vmstat
-    
-    printf "%s\n" "------------------------------"
-    printf "%s\n" " Top 10 Memory Eating Process "
-    printf "%s\n" "------------------------------"
-    printf "%s\n" "${processes}" | head -11 |awk '{print $1, $2, $3, $4, $5, $6, $7}'
-    
-    # pause
+vram_stats() {
+  # display virtual memory 
+
+  local vram=$(vmstat) 
+  write_info "Virtual Memory Statistics: ${vram}"
 }
 
-mem_info 
+# --> NEED TO FIX <--
 
+top_ram_eaters() {
+  # regex ps to define, extract, and sort top memory (then cpu) consuming processes  
+
+  local processes=$(ps -Ao user,pid,pcpu,pmem,stat,command --sort=-%mem,-%cpu \n
+  |head -11 | awk '{print $1, $2, $3, $4, $5, $6, $7}') 
+  write_info "Top 10 Memory Eating Process: ${processes}"
+}
 
 mem_info() { 
   # wrapper function 
 
   write_header "MEMORY INFO" "${mem_info}" 
 
-
+  ram_stats
+  vram_stats
+  top_ram_eaters
 }
 
 mem_info
+
+#### Display used and free memory info ####
+#function mem_info(){
+#    local processes=$(ps -Ao user,pid,pcpu,pmem,stat,command --sort=-%mem,-%cpu) 
+    # regex ps to define, extract, and sort top memory (then cpu) consuming processes  
+        
+#    write_header "Free & Used Memory "        
+#    free --giga --human                                                 # free -gh  
+    
+#    printf "%s\n" "------------------------------"
+#    printf "%s\n" "  Virtual Memory Statistics   "
+#    printf "%s\n" "------------------------------"
+#    vmstat
+    
+#    printf "%s\n" "------------------------------"
+#    printf "%s\n" " Top 10 Memory Eating Process "
+#    printf "%s\n" "------------------------------"
+#    printf "%s\n" "${processes}" | head -11 |awk '{print $1, $2, $3, $4, $5, $6, $7}'
+    
+    # pause
+#}
+
+#mem_info 
+
+
+
