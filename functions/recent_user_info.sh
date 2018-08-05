077ca8926ed018edfd163f2eaa5ba875393c6195
marshki <marshall@morethantomorrow.org>
2018-08-05 12:03:08 -0400

    rename function

---
+++ functions/recent_user_info.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ 
+#### Print header ####
+
+write_header() {
+  local name=$1; shift;
+  printf "%s""--------------------\\n$name%s\\n--------------------\\n"
+  printf "%s" "$@"
+}
+
+#### Display a list of recently logged in users ####
+
+ten_last() { 
+  # `last` built-in; last 10 incl. host & dns 
+
+  local lasty=$(last --limit 10 --hostlast --dns)  
+  printf "%s\\n" "${lasty}"   
+} 
+
+recent_users() { 
+  # wrapper function 
+
+  write_header "LAST 10 LOGINS" #"$user_info" 
+
+  ten_last 
+} 
+
+recent_users
