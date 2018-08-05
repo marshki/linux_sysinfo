8752c113d64e7fa7fb8b432b3d917f54b61bf7e2
marshki <marshall@morethantomorrow.org>
2018-08-05 11:58:06 -0400

    refactor

--- functions/net_info.sh
+++ functions/net_info.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Retrieve info about network interfaces & routing 
-
-# TODO 
-# Fix output for readability--this is an aesthetic & functional issue 
-# 08.01.2018 will revisit after refactoring other functions in this dir
+# TODO: 
+# add more descriptive comments 
 
 #### Print header ####
 
@@ -13,52 +11,50 @@ write_header() {
   printf "%s" "$@"
 }
 
-#### Print info ####
-
-write_info() {
-  local name=$1; shift;
-  printf "%s""$name%s"
-  printf "%s\\n" "$@"
-}
-
 #### Net Info ####
 
 network_interfaces() { 
   # Display number of network interfaces 
 
   local devices=$(ip -oneline link show |wc --lines) 
-  write_info "Total Network Interfaces Found: ${devices}"  	
+  printf "%s\\n" "${devices}"  	
 }
 
 ip_add_info() {
   # Display the IP protocol addresses
 
   local ip_add=$(ip --family inet address show)
-  write_info "IP Address Info: ${ip_add}"  
+  printf "%s\\n" "${ip_add}"  
 } 
 
 network_routing() {
   # Display the IP routing table  
 
   local route=$(netstat --route --numeric)
-  write_info "Network Routing: ${route}" 
+  printf "%s\\n" "${route}" 
 } 
 
 interface_traffic() {
   # Display status of interfaces on the network 
 
   local traffic=$(netstat --interfaces)
-  write_info "Interface Traffic Info: ${traffic}" 
+  printf "%s\\n" "Interface Traffic Info: ${traffic}" 
 } 
 
 net_info() {
   # wrapper function 
-  write_header "NETWORK INFO" "$net_info" 
 
-  network_interfaces
-  ip_add_info
-  network_routing
-  interface_traffic
+  write_header "TOTAL NETWORK INTERFACES FOUND:" 
+  network_interfaces  
+  
+  write_header "IP ADDRESS INFO:" 
+  ip_add_info 
+  
+  write_header "NETWORK ROUTING:" 
+  network_routing  
+  
+  write_header "INTERFACE TRAFFIC INFO:" 
+  interface_traffic 
 } 
  
 net_info
