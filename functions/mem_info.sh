a6609af806ffb29987d7a08941ba61743e3cd790
marshki <marshall@morethantomorrow.org>
2018-08-05 20:48:09 -0400

    refactor in progress

--- functions/mem_info.sh
+++ functions/mem_info.sh
@@ -1,7 +1,5 @@
 #!/bin/bash 
-
-# TODO: 
-# --> FIX ME, PLEASE!!! <--
+# Free and used memory info 
 
 #### Print header ####
 
@@ -11,46 +9,53 @@ write_header() {
   printf "%s" "$@"
 }
 
+# CAN PROBABLY REMOVE
 #### Print info ####
 
-write_info() {
-  local name=$1; shift;
-  printf "%s""$name%s"
-  printf "%s\\n" "$@"
-}
+#write_info() {
+#  local name=$1; shift;
+#  printf "%s""$name%s"
+#  printf "%s\\n" "$@"
+#}
 
-#### Describe function ####
+#### RAM Stats ####
 
 ram_stats() {
   # display free & used memory 
 
   local ram=$(free --giga --human) 
-  write_info "Free & Used Memory: ${ram}"
+  printf "%s\\n" "${ram}" 
 }
 
 vram_stats() {
   # display virtual memory 
 
   local vram=$(vmstat) 
-  write_info "Virtual Memory Statistics: ${vram}"
+  printf "$s\\n" "${vram}"
 }
-
-# --> NEED TO FIX <--
 
 top_ram_eaters() {
   # regex ps to define, extract, and sort top memory (then cpu) consuming processes  
 
-  local processes=$(ps -Ao user,pid,pcpu,pmem,stat,command --sort=-%mem,-%cpu |head -11 |awk '{print $1, $2, $3, $4, $5, $6, $7}') 
-  write_info "Top 10 Memory Eating Process: ${processes}"
+  local processes=$(ps -Ao user,pid,pcpu,pmem,stat,command --sort=-%mem,-%cpu \
+  |head -11 |awk '{print $1, $2, $3, $4, $5, $6, $7}') 
+  
 }
+
+#"${mem_info}" 
 
 mem_info() { 
   # wrapper function 
 
-  write_header "MEMORY INFO" "${mem_info}" 
+  write_header "MEMORY INFO" 
 
+  write_header "FREE & USED MEMORY"
   ram_stats
+  
+  write_header "VIRTUAL MEMEORY STATISTICS" 
   vram_stats
+
+  write_header "TOP 10 MEMORY EATING PROCESS" 
   top_ram_eaters
 }
 
