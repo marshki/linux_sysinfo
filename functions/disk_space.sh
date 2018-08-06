a0f213d367e1500a63ccafa7a8f07a3c031893e3
marshki <marshall@morethantomorrow.org>
2018-08-06 11:40:07 -0400

    re-factor complete (me thinks)

--- functions/disk_space.sh
+++ functions/disk_space.sh
@@ -18,18 +18,19 @@ disk_usage() {
   local disk=$(df --human-readable --total |awk 'NR==1; END{print}')
   printf "%s\\n" "${disk}"
 }
+# Top 10 disk-eating files 
 
-#disk_hogs() {
-  # Retrieve top 10 disk-eating files   
+  # Retrieve top 10 disk-eating files 
 
-  # scan file system from / for files ; output background noise to /dev/null; pv for progress 
-  #local sort_files=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | pv)  
+disk_hogs() { 
+ 
+  printf "%s\\n" "Searching..." 
   
-  # take output from sort_files; show progress; sort files; show top 10 
-  #local top_files$("${sort_files}" | pv | sort --reverse --human | head --lines=10 ) 
-  
-  #write_info "Top 10 Disk Eating Files: ${disk_hogs}"
-#}
+  # scan file system from / for files; output background noise to /dev/null; pv for progress 
+  local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null |pv)
+
+  printf "%s\n" "${largestfiles}" |sort --reverse --human | head --lines=10
+} 
 
 disk_space() { 
   # wrapper function 
@@ -38,26 +39,9 @@ disk_space() {
 
   write_header "DISK USAGE" 
   disk_usage
-  # disk_hogs  
+
+  write_header "TOP 10 DISK-EATING FILES" 
+  disk_hogs  
 }
 
 disk_space
-
-#    printf "%s\n" "Searching..." 
-
-#    local largestfiles=$(find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | pv)
-    
-    # find largest files by disk space; output background noise to /dev/null 
-    # find / -type f -exec du -Sh {} + 2>/dev/null
-             
-#    write_header "Disk Usage" 
-#    df --human-readable --total | awk 'NR==1; END{print}'                                          # df -h --total   
-        
-#    printf "%s\n" "------------------------------" 
-#    printf "%s\n" "   Top 10 Disk Eating Files   " 
-#    printf "%s\n" "------------------------------"     
-#    printf "%s\n" "${largestfiles}" | pv | sort --reverse --human | head --lines=10                # sort -rh | head -n 10
- 
-    # find / -type f -exec du --separate-dirs --human-readable {} + 2>/dev/null | sort --reverse --human | head --lines=10 
-    # find / -type f -exec du -Sh {} + 2>/dev/null | sort -rh | head -n 10
-#}
