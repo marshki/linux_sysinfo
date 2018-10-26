84e72dd60b4ebd6850cc488a0699c4c735c7d5a2
marshki <marshall@morethantomorrow.org>
2018-10-26 19:51:36 -0400

    disk sort by folder size: testing

--- functions/disk_folder.sh
+++ functions/disk_folder.sh
@@ -6,4 +6,18 @@ write_info() {
   local name=$1; shift; 
   printf "%s" "$name%s" 
   printf "%s\\n" "$@"  
+}
+
+disk_hogs() {
+  # Retrieve top 10 disk-eating directories
+
+  printf "%s\\n" "Searching..."
+
+  # scan file system from / for directories; output background noise to: /dev/null; pv for progress 
+  local largestdirs=$(find / -type d -exec du --separate-dirs --human-readable {} + 2>/dev/null |pv)
+
+  printf "%s\\n" "${largestdirs}" |sort --reverse --human |head --lines=10 
+
 } 
+
+disk_hogs 
