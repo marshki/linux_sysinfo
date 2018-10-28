fbf55e3715376a1cec3c6984794863422bab3094
marshki <marshall@morethantomorrow.org>
2018-10-28 11:47:05 -0400

    update

--- functions/top_10_dirs.sh
+++ functions/top_10_dirs.sh
@@ -16,6 +16,10 @@ disk_hogs() {
   # scan file system from / for directories; output background noise to: /dev/null; pv for progress 
   local largestdirs=$(find / -type d -exec du --max-depth=2 --human-readable {} + 2>/dev/null |pv)
   
+  # local largestdirs=$(find / -type d -exec du --separate-dirs --human-readable {} + 2>/dev/null |pv)
+  
+  #local largestdirs=$(find /home -type d -exec du --separate-dirs --human-readable {} + 2>/dev/null |pv) 
+
   printf "%s\\n" "${largestdirs}" |sort --reverse --human |head --lines=10 
 
 } 
