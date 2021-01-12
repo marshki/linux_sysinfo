84e70061bf8c72b4978807ebfce3769cd6e659ca
marshki <marshall@morethantomorrow.org>
2021-01-12 19:16:13 +0000

    net-tools is utility that includes netstat

--- functions/net-tools_check.sh
+++ functions/net-tools_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Check if netstat is an installed package
+# Check if netstat is an installed package.
 
 function netstat_check(){
   if ! which netstat &> /dev/null; then
-  printf "%s\n" "ERROR: Please install netstat or make sure it is in your path."
+  printf "%s\n" "ERROR: Please install net-tools or make sure it is in your path."
   exit 1
 fi
 }
